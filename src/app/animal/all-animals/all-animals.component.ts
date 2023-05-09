39ffe01e168ea297ed837acc248e8dee79120739
haris.khan636 <harisjriv@gmail.com>
2023-05-09 18:52:55 +0500

    initial ui and logic

--- src/app/animal/all-animals/all-animals.component.ts
+++ src/app/animal/all-animals/all-animals.component.ts
@@ -3,13 +3,37 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-all-animals',
   templateUrl: './all-animals.component.html',
-  styleUrls: ['./all-animals.component.css']
+  styleUrls: ['./all-animals.component.css'],
 })
 export class AllAnimalsComponent implements OnInit {
+  animalList = [
+    {
+      name: 'Meowsy',
+      species: 'cat',
+      foods: {
+        likes: ['tuna', 'catnip'],
+        dislikes: ['ham', 'zucchini'],
+      },
+    },
+    {
+      name: 'Barky',
+      species: 'dog',
+      foods: {
+        likes: ['bones', 'carrots'],
+        dislikes: ['tuna'],
+      },
+    },
+    {
+      name: 'Purrpaws',
+      species: 'cat',
+      foods: {
+        likes: ['mice'],
+        dislikes: ['cookies'],
+      },
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
